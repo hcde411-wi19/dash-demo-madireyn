b1b590b88c4e18c25b3ff2321341726988095dc4
Madi <madireyn@D-10-19-24-216.dhcp4.washington.edu>
2019-03-12 14:45:36 -0700

    changing herokuapp to show ex2 instead of ex1

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 # Note: import the app of what you are working on
 # from vis_line_chart import app
-from exercise1 import app
-# from exercise2 import app
-# from exercise2 import app
+# from exercise1 import app
+from exercise2 import app
 
 server = app.server
 
