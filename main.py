d11c9bf561a2fd6ecc51d09060cc5af4333c198c
Madi <madireyn@D-10-19-24-216.dhcp4.washington.edu>
2019-03-12 14:07:06 -0700

    updating to exercise1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # Note: import the app of what you are working on
-from vis_line_chart import app
-# from exercise1 import app
+# from vis_line_chart import app
+from exercise1 import app
 # from exercise2 import app
 # from exercise2 import app
 
